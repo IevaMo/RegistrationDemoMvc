b81b9849a646f20b9efd5662a8d7a73187906666
IevaMo <mockute1@gmail.com>
2020-12-23 16:09:08 +0100

    2020.12.23 namų darbai.

--- RegistrationDemoMvc.WebApp/Models/RegistrationModel.cs
+++ RegistrationDemoMvc.WebApp/Models/RegistrationModel.cs
@@ -1,8 +1,20 @@
-﻿namespace RegistrationDemoMvc.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationDemoMvc.WebApp.Models
 {
     public class RegistrationModel
     {
+        [Required(ErrorMessage = "The Name field is required.")]
         public string Name { get; set; }
+
+        [Required]
+        [Range(1, 110)]
+        [Display(Name = "Age")]
         public string Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
     }
 }
